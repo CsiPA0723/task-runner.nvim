73fa0d0dd61486417f441e8f0ded268b97f908b4
Péter Csizmadia <csizipeti0723@gmail.com>
2025-04-19 02:13:09 +0200

    Add example

---
+++ examples/test1.lua
@@ -0,0 +1,15 @@
+---@type TaskRunner.ModuleConfig
+return {
+	name = 'Testing_Module',
+	tasks = {
+		asder = {
+			command = 'echo',
+			args = { 'asd' },
+		},
+		list = {
+			command = 'ls',
+			args = { '-la' },
+			cwd = '~/.config/nvim',
+		},
+	},
+}
