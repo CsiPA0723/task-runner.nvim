990239e816aff81e824785d40b0ca11dedff4157
Péter Csizmadia <csizipeti0723@gmail.com>
2025-05-12 09:15:30 +0200

    Framework for pickers

--- lua/task-runner/picker.lua
+++
@@ -1,5 +0,0 @@
-local M = {}
-
-function M.open() end
-
-return M
---
+++ lua/task-runner/task/picker.lua
@@ -0,0 +1,11 @@
+local M = {}
+
+function M.open() end
+
+local provider_modules = {
+	snacks = 'snacks.picker',
+	telescope = 'telescope.pickers',
+	fzf_lua = 'fzf_lua',
+}
+
+return M
